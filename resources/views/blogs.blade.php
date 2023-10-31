0e5e0c179e2639231b37ad60d18670b196b30085
MinKhant610 <minkhantiopp@gmail.com>
2023-10-31 14:51:00 +0630

    first commit

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel = "stylesheet" href = "/css/app.css">
+</head>
+<body>
+    <article>
+        <h1> <a href="/blog">First blog</a> </h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus doloremque beatae libero omnis modi repellendus tempora, asperiores veritatis ratione ad voluptate! Libero aspernatur porro autem sint assumenda quos asperiores esse quam ipsum adipisci. Adipisci sint suscipit aspernatur laboriosam harum. Quae eveniet possimus velit dolore, laudantium pariatur accusantium asperiores qui ratione magnam in ad quo beatae saepe dolor sint ipsum similique nostrum, odit quia provident dolorem enim? Similique nesciunt illo quidem aliquid aut voluptatem nam ex. Aspernatur iure, fuga dignissimos fugiat illo ipsa dolor assumenda. Praesentium consequatur, eveniet est ab eius ex dolores corporis quia maxime dicta explicabo harum magni cum!</p>
+    </article>
+    <article>
+        <h1> <a href="/blog">Second blog</a> </h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus doloremque beatae libero omnis modi repellendus tempora, asperiores veritatis ratione ad voluptate! Libero aspernatur porro autem sint assumenda quos asperiores esse quam ipsum adipisci. Adipisci sint suscipit aspernatur laboriosam harum. Quae eveniet possimus velit dolore, laudantium pariatur accusantium asperiores qui ratione magnam in ad quo beatae saepe dolor sint ipsum similique nostrum, odit quia provident dolorem enim? Similique nesciunt illo quidem aliquid aut voluptatem nam ex. Aspernatur iure, fuga dignissimos fugiat illo ipsa dolor assumenda. Praesentium consequatur, eveniet est ab eius ex dolores corporis quia maxime dicta explicabo harum magni cum!</p>
+    </article>
+    <article>
+        <h1> <a href="/blog">Third blog</a> </h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus doloremque beatae libero omnis modi repellendus tempora, asperiores veritatis ratione ad voluptate! Libero aspernatur porro autem sint assumenda quos asperiores esse quam ipsum adipisci. Adipisci sint suscipit aspernatur laboriosam harum. Quae eveniet possimus velit dolore, laudantium pariatur accusantium asperiores qui ratione magnam in ad quo beatae saepe dolor sint ipsum similique nostrum, odit quia provident dolorem enim? Similique nesciunt illo quidem aliquid aut voluptatem nam ex. Aspernatur iure, fuga dignissimos fugiat illo ipsa dolor assumenda. Praesentium consequatur, eveniet est ab eius ex dolores corporis quia maxime dicta explicabo harum magni cum!</p>
+    </article>
+</body>
+</html>
