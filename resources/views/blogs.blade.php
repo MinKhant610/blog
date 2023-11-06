0570e7bb2e0ebd9e5b95657819b32583dc00dbc1
MinKhant610 <minkhantiopp@gmail.com>
2023-11-07 00:03:27 +0630

    update design

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,28 +1,9 @@
 <x-layout>
-    <x-slot name="title">
-        <title>All Blogs</title>
-    </x-slot>
-    @foreach($blogs as $blog)
-    <div>
-        <h1><a href="blogs/{{$blog->slug}}">
-                {{$blog->title}}
-            </a>
-        </h1>
-        <h4>
-            <a href="/users/{{$blog->author->username}}">Author - {{$blog->author->username}}</a>
-        </h4>
-        <p>
-            <a href="/categories/{{$blog->category->slug}}">{{$blog->category->name}}</a>
-        </p>
-        <div>
-            <p>
-                published at -
-                {{$blog->created_at->diffForHumans()}}
-            </p>
-            <p>
-                {{$blog->intro}}
-            </p>
-        </div>
-    </div>
-    @endforeach
+    <!-- hero section -->
+    <x-hero></x-hero>
+
+    <!-- blogs section -->
+    <x-blogs-section></x-blogs-section>
+    <!-- subscribe new blogs -->
+        <x-subscribe></x-subscribe>
 </x-layout>
