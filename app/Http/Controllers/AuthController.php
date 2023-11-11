779757eefd8cfc8098cc971c691b9a7b745a789f
MinKhant610 <minkhantiopp@gmail.com>
2023-11-11 16:38:25 +0630

    add register form and validation

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function create(){
+        return view('register.create');
+    }
+
+    public function store(){
+        //validation
+        request()->validate([
+            'name' => ['required', 'max:255', 'min:3'],
+            'username' => ['required', 'max:255', 'min:3'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8']
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
@@ -7,3 +8,6 @@ use App\Models\User;
 Route::get('/', [BlogController::class, 'index']);
 //                    wildcard          wildcard parm
 Route :: get ('/blogs/{blog:slug}', [BlogController::class, 'show'])-> where('blog', '[A-Za-z\d\-_]+');
+
+Route::get('/register',[AuthController::class, 'create']);
+Route::post('/register',[AuthController::class, 'store']);
